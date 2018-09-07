98340a68aa6b26ec449a13cd82ddc788d1b53db6
Alexei macbook air <Alexeimacbookair@Alexei-MacBook-Air.local>
2018-09-07 21:44:55 +0300

    My first commit

---
+++ test.py
@@ -0,0 +1,68 @@
+print("hello, world")
+
+variable1 = 2
+variable2 = 1
+buffer = variable2
+variable2 = variable1
+variable1=buffer
+print(variable1, variable2)
+value = 11111
+print(value)
+float_number=123.1
+complex_number=123+123j
+string1="double quote"
+string2='single quote'
+string3="""triple quote"""
+string4='''triple quote'''
+
+#operators
+a=1
+b=266
+print('numbers', a, b)
+print('+', a+b)
+print('%', a % b)
+print('numbers', b, a)
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
+
+
+a=4
+b=5
+print ('&', a&b)
+
+
+#operators
+a = 1234
+b = 3453
+print(a, b)
+print('<', a<b)
+print('<=', a<=b)
+
+print(True, False)
+print(None)
+print('compare string with False', '' == False)
+
+if True:
+    a = 1
+    b = 2
+    c = 3
+elif False or True:
+    c = 1
+elif False or True or False:
+    d = 1
+else:
+    b = 1
+print ("Information")
+a = input ("your name")
+print(a)
+
+
+
+
+n1 = int(input("figure1? "))
+n2 = int(input ("figure2 "))
+print (bool (n1 % n2))
+
+
